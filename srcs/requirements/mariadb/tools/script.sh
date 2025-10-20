5b559b657fc644a85409c56e6d90e325417330e4
CodeKiwiFR <madric.hottinger.101@gmail.com>
2025-10-20 04:48:53 +0200

    Modifies Dockerfile and script for mariadb

--- srcs/requirements/mariadb/tools/script.sh
+++ srcs/requirements/mariadb/tools/script.sh
@@ -1,6 +1,27 @@
-#!/bin/sh
+#!/bin/bash
 
-# Stop if a command encounters an error
-set -e
+# --- Variables pour le test ---
+export MYSQL_ROOT_PASSWORD=root_pass
+export MYSQL_DATABASE=testdb
+export MYSQL_USER=testuser
+export MYSQL_PASSWORD=testpass
 
-if [ ! -d /var/lib/mysql/mysql ];
+echo "[INFO] Ensuring database and user exist..."
+
+# Start MariaDB in the background
+mysqld_safe --skip-networking &
+sleep 10
+
+# Create DB and user
+mysql -u root -p"${MYSQL_ROOT_PASSWORD}" <<EOF
+CREATE DATABASE IF NOT EXISTS \`${MYSQL_DATABASE}\`;
+CREATE USER IF NOT EXISTS '${MYSQL_USER}'@'%' IDENTIFIED BY '${MYSQL_PASSWORD}';
+GRANT ALL PRIVILEGES ON \`${MYSQL_DATABASE}\`.* TO '${MYSQL_USER}'@'%';
+FLUSH PRIVILEGES;
+EOF
+
+# Shut down temporary instance
+mysqladmin -u root -p"${MYSQL_ROOT_PASSWORD}" shutdown
+
+# Start MariaDB normally
+exec mysqld_safe
